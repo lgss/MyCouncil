cb211d4496bb272bd931fb58f2c1b5c8253f1e6f
Kevin White <kevin.white@clubpit.com>
2010-10-02 16:11:16 +0000

    Tidying up code.

--- PWCallback.java
+++ PWCallback.java
@@ -6,20 +6,13 @@ import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
 import org.apache.ws.security.WSPasswordCallback;
 
-/**
- * PWCallback for the Client
- */
 public class PWCallback implements CallbackHandler {
 
-    /**
-     * @see javax.security.auth.callback.CallbackHandler#handle(javax.security.auth.callback.Callback[])
-     */
     public void handle(Callback[] callbacks) throws IOException,
                     UnsupportedCallbackException {
         for (int i = 0; i < callbacks.length; i++) {
             if (callbacks[i] instanceof WSPasswordCallback) {
                 WSPasswordCallback pc = (WSPasswordCallback)callbacks[i];
-                // set the password given a username
                 if ("MyCouncil".equals(pc.getIdentifier())) 
 				{
                     pc.setPassword("password");
